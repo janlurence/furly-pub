7d1a6425b96590ad5b8ba03f4edea1a427e7c9f5
Jan <ua.janlurence@gmail.com>
2026-07-06 12:33:08 +0800

    Deploy

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = 'furly-v1'
+const CACHE = 'furly-v2'
+const IMAGE_CACHE = 'furly-images-v1'
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/furly.svg']
 
 self.addEventListener('install', (e) => {
@@ -8,14 +9,34 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => ![CACHE, IMAGE_CACHE].includes(k)).map((k) => caches.delete(k)))
+    )
   )
   self.clients.claim()
 })
 
 self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url)
-  if (e.request.method !== 'GET' || url.origin !== self.location.origin) return
+  if (e.request.method !== 'GET') return
+
+  if (e.request.destination === 'image') {
+    e.respondWith(
+      caches.open(IMAGE_CACHE).then((cache) =>
+        cache.match(e.request).then(
+          (cached) =>
+            cached ||
+            fetch(e.request).then((res) => {
+              if (res.ok || res.type === 'opaque') cache.put(e.request, res.clone())
+              return res
+            })
+        )
+      )
+    )
+    return
+  }
+
+  if (url.origin !== self.location.origin) return
   // Network-first for navigation, cache-first for static assets
   if (e.request.mode === 'navigate') {
     e.respondWith(
